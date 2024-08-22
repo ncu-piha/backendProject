c07383ae3526880ba43d5670214c22c868b10ffa
ncu-piha <piha21csu473@ncuindia.edu>
2024-08-22 22:37:15 +0530

    utils code

---
+++ src/index.js
@@ -0,0 +1,40 @@
+
+//require('dotenv').config()
+import dotenv from "dotenv";
+import connectDB from "../db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+//beacuse yeh async hai to promise return krega to we can use .then and catch method 
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on port:${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("mongodb connection failed",err);
+})
+
+
+
+
+//1st approach
+//iife use kro 
+// (async()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//        app.on("error",(error)=>{
+//         console.log("err:",error);
+//         throw error
+//        })
+//        app.listen(process.env.PORT,()=>{
+//         console.log(`App listening on port ${process.env.PORT}`)
+//        })
+//     } catch (error) {
+//         console.error("ERROR:",error)
+//     }
+// })()
