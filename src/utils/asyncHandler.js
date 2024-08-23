c617add59b05c59996240dc8670ef4e38ca10bd7
ncu-piha <piha21csu473@ncuindia.edu>
2024-08-24 00:31:43 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
-
+//return karega promise (higher order function)
 const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
     }
 }
